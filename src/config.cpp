e57627e11599270371272aa8ae2281faa44f65d2
gab5987 <biell.gb4@gmail.com>
2022-12-24 17:04:45 -0300

    Feat: Config methods and config file

---
+++ src/config.cpp
@@ -0,0 +1,47 @@
+#include "main.hpp"
+
+ServerSetup::ServerSetup() {
+    Serial.begin(9600);
+
+    while (!sd.init()) {
+        Serial.println("SD Card Mount Failed");
+        delay(1000);
+    }
+
+
+    String config = sd.readFile(SD, "/config.conf");
+
+    std::vector<std::string> tokens;
+    std::stringstream ss(config.c_str());
+    std::string word;
+    while(ss >> word) {
+        if(word == " ") continue;
+        tokens.push_back(word);
+    }
+
+    for(auto i = 0; i < tokens.size(); i++) {
+        if(tokens[i] == "NETWORK_SSID") {
+            ssid = tokens[i+1].c_str();
+        } else if(tokens[i] == "NETWORK_PASSWORD") {
+            password = tokens[i+1].c_str();
+        }
+    }
+
+    if(ssid == "" || password == "") {
+        Serial.println("Error: setup file is not configured \n Check the configuration manual");
+        Serial.println("E: ssid or password is empty or does not exist");
+        exit(1);
+    }
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
-#include <Arduino.h>
+#include "main.hpp"
+
+WiFiServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
+  ServerSetup srv;
+
+  Serial.println(srv.sd.readFile(SD, "/testeSdCard.txt"));
+
+  server.begin();
+
+  // sd.writeFile(SD, "/testeSdCard.txt", "sdjhgjcsdhgfjhdgfjhgjeg \r\n");
+  // sd.appendFile(SD, "/testeSdCard.txt", "teste 12349843jfhbdsjhbfjhv\r\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
+  WiFiClient client = server.available();
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close"); // the connection will be closed after completion of the response
+  // client.println("Refresh: 5");        // refresh the page automatically every 5 sec
+  client.println();
+  client.println("<!DOCTYPE html><html><head><title>ESP32 Web Server</title><meta charset='UTF-8'>");
+  client.println("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'>");
+  client.println("<body> \
+    <style> \
+        body { \
+            background-color: #f5f5f5; \
+            max-height: 100vh; \
+            max-width: 100vw; \
+        }\
+    </style>");
+  client.println("<div class='container'> \
+    <div class='row'> \
+        <div class='col-12'> \
+            <h1 class='text-center'>ESP32 Web Server</h1> \
+        </div> \
+    </div> \
+    <div class='row'> \
+        <div class='col-12'> \
+            <h2 class='text-center'> </h2> \
+        </div> \
+    </div> \
+    <div class='row'> \
+        <div class='col-12'> \
+            <h3 class='text-center'>");
+
+  client.println("</body> \
+    </html>");
 }
