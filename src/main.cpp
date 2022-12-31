b8cc427060015aff4623a260f30cfcc4a9e03ace
gab5987 <biell.gb4@gmail.com>
2022-12-31 12:00:22 -0300

    Feat: Cors for browsers

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,22 @@
 // creates a web server instance
 AsyncWebServer server(80);
 
+String stringfy(String str) { return "\"" + str + "\""; }
+
 // function and macro to create a JSON with the system status
 #define addJson(var, key, value) var += String("\"" + String(key) + "\":" + String(value) + ",")
 String getSystemStatus() {
   String j = "{";
-  addJson(j ,"freeHeap", ESP.getFreeHeap());
-  addJson(j, "chipRevision", ESP.getChipRevision());
-  addJson(j, "macAddress", WiFi.macAddress());
-  addJson(j, "rssi", WiFi.RSSI());
-  addJson(j, "bssid", WiFi.BSSIDstr());
-  addJson(j, "channel", WiFi.channel());
-  addJson(j, "subnetMask", WiFi.subnetMask());
-  addJson(j, "gatewayIP", WiFi.gatewayIP());
-  addJson(j, "dnsIP", WiFi.dnsIP());
+  addJson(j ,"freeHeap", stringfy(String(ESP.getFreeHeap())));
+  addJson(j, "chipRevision", stringfy(String(ESP.getChipRevision())));
+  addJson(j, "macAddress", stringfy(WiFi.macAddress()));
+  addJson(j, "rssi", stringfy(String(WiFi.RSSI())));
+  addJson(j, "bssid", stringfy(String(WiFi.BSSIDstr())));
+  addJson(j, "channel", stringfy(String(WiFi.channel())));
+  addJson(j, "subnetMask", stringfy(String(WiFi.subnetMask())));
+  addJson(j, "gatewayIP", stringfy(String(WiFi.gatewayIP())));
+  addJson(j, "dnsIP", stringfy(String(WiFi.dnsIP())));
+  j+= "\"placeholder\": \"loremIpsum\"";
   j += "}";
   return j;
 }
@@ -30,10 +33,14 @@ void setup() {
   ServerSetup srv; // initiate the server (see on config.cpp)
 
   db.begin(); // initiate the database
+
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type");
   
   // This route returns a webpage with the system status, such as free heap, mac address, etc.
   // Also contains a list of all the devices in the database and its status
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SD, "/mdk512/web/index/index.html", "text/html");
   });
 
